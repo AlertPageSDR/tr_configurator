f9bf7a82d677b5b632bbac1b533b50d8c8a26273
Max Watermolen <mwateremolen@maxwelldps.com>
2022-04-29 11:53:24 -0500

    modulation fixes

--- main.py
+++ main.py
@@ -488,10 +488,11 @@ def main():
         
                 site_type = system_types[system["system_type"]]
                 if site_type == "p25":
-                    if site["modulation"] == "CPQSK":
-                        modulation = "qpsk"
-                    else:
+                    modulation = "qpsk"
+                    if not site["modulation"] == "CPQSK":
                         modulation = "fsk4"
+                    else:
+                        modulation = "qpsk"
                 else:
                     modulation = "fsk4"
 
@@ -554,10 +555,11 @@ def main():
                 
                 site_type = system_types[system["system_type"]]
                 if site_type == "p25":
-                    if site["modulation"] == "CPQSK":
-                        modulation = "qpsk"
-                    else:
+                    modulation = "qpsk"
+                    if not site["modulation"] == "CPQSK":
                         modulation = "fsk4"
+                    else:
+                        modulation = "qpsk"
                 else:
                     modulation = "fsk4"
 
