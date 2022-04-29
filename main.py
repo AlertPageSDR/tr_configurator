8555f70a454f49f23662dd52f08a41b69ce9494b
Max Watermolen <mwateremolen@maxwelldps.com>
2022-04-29 11:37:52 -0500

    fixed string

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class RR:
             talkgroup_categories = json.loads(json.dumps(helpers.serialize_object(talkgroup_categories, dict), cls=DecimalEncoder))
 
         if add_metadata:
-            logging.warning("[+] Fetching Radio Reference data, this will take a hot sec...")
+            logging.warning("[+] Fetching Radio Reference data, this will take a hot sec, so go airfry a hot-pocket or some pizza rolls")
             talkgroups = []
             for talkgroup in json.loads(json.dumps(talkgroups_result, cls=DecimalEncoder)):
                 for cat in talkgroup_categories:
